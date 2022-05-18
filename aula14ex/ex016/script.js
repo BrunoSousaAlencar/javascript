8fec292f6c61b56266d224aa320a2ce8b810ae08
Bruno e Sousa Alencar <brunoeesousanoleto2017@gmail.com>
2022-05-18 15:56:39 -0300

    .

---
+++ aula14ex/ex016/script.js
@@ -0,0 +1,13 @@
+function contar(){
+    var inicio = window.document.getElementById('inicio')
+    var fim = window.document.getElementById('fim')
+    var passo = window.document.getElementById('passo')
+    var c = 0
+    console.log("Olá, Mundo!")
+
+    for(c = inicio; c <= fim; passo++){
+        var res = passo
+    }
+
+    
+}
