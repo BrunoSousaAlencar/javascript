92f6408534acba31616c1710f71e74de8bb841a1
Bruno e Sousa Alencar <brunoeesousanoleto2017@gmail.com>
2022-05-19 16:06:49 -0300

    contador finalizado

--- aula14ex/ex016/script.js
+++ aula14ex/ex016/script.js
@@ -1,13 +1,32 @@
 function contar(){
-    var inicio = window.document.getElementById('inicio')
-    var fim = window.document.getElementById('fim')
-    var passo = window.document.getElementById('passo')
-    var c = 0
-    console.log("Olá, Mundo!")
+    let inicio = document.getElementById('txti')
+    let fim = document.getElementById('txtf')
+    let passo = document.getElementById('txtp')
+    let res = document.getElementById('res')
 
-    for(c = inicio; c <= fim; passo++){
-        var res = passo
-    }
+    if (inicio.value.length == 0 || fim.value.length == 0 || passo.value.length == 0){
+        res.innerHTML ="Impossível contar!"
+        window.alert('[ERRO] Verifique os dados!')
+    } else{
+        res.innerHTML = "Contando: <br>"
+        let i = Number(inicio.value)
+        let f = Number(fim.value)
+        let p = Number(passo.value)
 
-    
+        if(p <= 0) {
+            window.alert('Passo inválido! Considerando PASSO 1')
+            p = 1
+        }
+
+        if (i < f ){
+            for(let c = i; c <= f; c += p){
+                res.innerHTML += `${c} \u{1F449}`
+            }
+        } else{
+            for(let c = i; c >= f; c -= p){
+                res.innerHTML += `${c} \u{1F449}`
+            }
+        }
+        res.innerHTML += `\u{1F3C1}`
+    } 
 }
